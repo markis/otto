bc28961f5905c4eb2b9220f19d46386b41027271
Markis Taylor <m@rkis.cc>
2021-01-02 17:42:28 -0500

    Refactor discord client to be class-based

--- otto/discord.py
+++ otto/discord.py
@@ -22,132 +22,122 @@ from otto import DISCORD_TOKEN
 from otto import SUBREDDIT_NAME
 from otto.handlers import execute_command
 
-client = Client()
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logger = logging.getLogger(__name__)
-process_list: Deque[str] = deque(maxlen=10)
+logger = logging.getLogger("otto.discord")
+# Z_SYNC_FLUSH suffix
+ZLIB_SUFFIX = b"\x00\x00\xff\xff"
 
 
-def _parse_request(message: Message) -> Tuple[str, str, str, str]:
-    if message:
-        msg_id = message.id
-        msg_text = message.content
-        channel_url = message.channel
-        username = message.author.name
+class DiscordClient(Client):  # type: ignore
+    msg_buffer = bytearray()
+    inflator = zlib.decompressobj()
+    process_list: Deque[str] = deque(maxlen=10)
 
-    return msg_id, msg_text, channel_url, username
+    def _parse_request(self, message: Message) -> Tuple[str, str, str, str]:
+        if message:
+            msg_id = message.id
+            msg_text = message.content
+            channel_url = message.channel
+            username = message.author.name
 
+        return msg_id, msg_text, channel_url, username
 
-def _parse_raw_request(message: Dict[str, Any]) -> Tuple[str, str, str, str]:
-    if message:
-        message_data = message["d"]
-        if message_data:
-            msg_id = message_data["id"]
-            channel_id = message_data["channel_id"]
-            username = message_data["member"]["user"]["username"]
+    def _parse_interaction(self, message: Dict[str, Any]) -> Tuple[str, str, str, str]:
+        if message:
+            message_data = message["d"]
+            if message_data:
+                msg_id = message_data["id"]
+                channel_id = message_data["channel_id"]
+                username = message_data["member"]["user"]["username"]
 
-            data = message_data["data"]
-            msg_text = f"/{data['name']}"
-            for option in data.get("options", []):
-                msg_text += f" --{option['name']}={option['value']}"
+                data = message_data["data"]
+                msg_text = f"/{data['name']}"
+                for option in data.get("options", []):
+                    msg_text += f" --{option['name']}=\"{option['value']}\""
 
-    return msg_id, msg_text, channel_id, username
+        return msg_id, msg_text, channel_id, username
 
+    def asyncio_run(self, future: Any, as_task: bool = True) -> Any:
+        """
+        A better implementation of `asyncio.run`.
 
-def asyncio_run(future: Any, as_task: bool = True) -> Any:
-    """
-    A better implementation of `asyncio.run`.
+        :param future: A future or task or call of an async method.
+        :param as_task: Forces the future to be scheduled as task (needed for e.g. aiohttp).
+        """
 
-    :param future: A future or task or call of an async method.
-    :param as_task: Forces the future to be scheduled as task (needed for e.g. aiohttp).
-    """
-
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:  # no event loop running:
-        loop = asyncio.new_event_loop()
-        return loop.run_until_complete(_to_task(future, as_task, loop))
-    else:
-        nest_asyncio.apply(loop)
-        return asyncio.run_coroutine_threadsafe(future, loop)
-
-
-def _to_task(future: Any, as_task: bool, loop: Any) -> Any:
-    if not as_task or isinstance(future, asyncio.Task):
-        return future
-    return loop.create_task(future)
-
-
-_buffer = bytearray()
-_zlib = zlib.decompressobj()
-
-
-@client.event
-async def on_socket_raw_receive(msg: Message) -> None:
-    logger.info("raw_message")
-    global _buffer
-
-    if type(msg) is bytes:
-        _buffer.extend(msg)
-
-    if len(msg) >= 4:
-        if msg[-4:] == b"\x00\x00\xff\xff":
-            try:
-                msg = _zlib.decompress(_buffer)
-                msg = msg.decode("utf-8")
-            finally:
-                _buffer = bytearray()
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:  # no event loop running:
+            loop = asyncio.new_event_loop()
+            return loop.run_until_complete(self._to_task(future, as_task, loop))
         else:
+            nest_asyncio.apply(loop)
+            return asyncio.run_coroutine_threadsafe(future, loop)
+
+    def _to_task(self, future: Any, as_task: bool, loop: Any) -> Any:
+        if not as_task or isinstance(future, asyncio.Task):
+            return future
+        return loop.create_task(future)
+
+    async def on_socket_raw_receive(self, msg_bytes: bytes) -> None:
+        logger.info("on_socket_raw_receive")
+
+        if type(msg_bytes) is bytes:
+            self.msg_buffer.extend(msg_bytes)
+
+        if len(msg_bytes) < 4 or msg_bytes[-4:] != ZLIB_SUFFIX:
             return
-    else:
-        return
 
-    msg = json.loads(msg)
-    if msg.get("t") == "INTERACTION_CREATE":
-        msg_id, msg_text, channel_id, username = _parse_raw_request(msg)
+        try:
+            msg_bytes = self.inflator.decompress(self.msg_buffer)
+            msg_str = msg_bytes.decode("utf-8")
+        finally:
+            self.msg_buffer = bytearray()
 
-        channel = client.get_channel(channel_id)
-        if not channel:
-            channel = await client.fetch_channel(channel_id)
+        msg = json.loads(msg_str)
+        if msg.get("t") == "INTERACTION_CREATE":
+            msg_id, msg_text, channel_id, username = self._parse_interaction(msg)
+
+            channel = client.get_channel(channel_id)
+            if not channel:
+                channel = await client.fetch_channel(channel_id)
+
+            def _send_message(response: str) -> None:
+                self.asyncio_run(channel.send(response))
+
+            execute_command(
+                cmd_str=f"otto {msg_text}",
+                sr_name=SUBREDDIT_NAME,
+                send_message=_send_message,
+                username=username,
+            )
+
+    async def on_message(self, message: Message) -> None:
+        logger.info("on_message")
+        sr_name = SUBREDDIT_NAME
+        msg_id, msg_text, channel_url, username = self._parse_request(message)
+        logger.debug(f"{msg_id}, {msg_text}")
+
+        if message.author == client.user:
+            return
+
+        if msg_id in self.process_list:
+            return
+
+        if not msg_text or not msg_text.startswith("/"):
+            return
+
+        self.process_list.append(msg_id)
 
         def _send_message(response: str) -> None:
-            asyncio_run(channel.send(response))
+            self.asyncio_run(message.channel.send(response))
 
         execute_command(
             cmd_str=f"otto {msg_text}",
-            sr_name=SUBREDDIT_NAME,
+            sr_name=sr_name,
             send_message=_send_message,
             username=username,
         )
-
-
-@client.event
-async def on_message(message: Message) -> None:
-    logger.info("handling discord message")
-    sr_name = SUBREDDIT_NAME
-    msg_id, msg_text, channel_url, username = _parse_request(message)
-    logger.debug(f"{msg_id}, {msg_text}")
-
-    if message.author == client.user:
-        return
-
-    if msg_id in process_list:
-        return
-
-    if not msg_text or not msg_text.startswith("/"):
-        return
-
-    process_list.append(msg_id)
-
-    def _send_message(response: str) -> None:
-        asyncio_run(message.channel.send(response))
-
-    execute_command(
-        cmd_str=f"otto {msg_text}",
-        sr_name=sr_name,
-        send_message=_send_message,
-        username=username,
-    )
 
 
 def update_slash_commands() -> None:
@@ -203,4 +193,5 @@ def update_slash_commands() -> None:
 
 if __name__ == "__main__":
     update_slash_commands()
+    client = DiscordClient()
     client.run(DISCORD_TOKEN)
