69e3653e139918c02f39d37360d26141ff568a59
Markis Taylor <m@rkis.cc>
2021-01-14 21:32:05 -0500

    Separate posts stream and jobs

--- otto/job.py
+++ otto/job.py
@@ -1,4 +1,3 @@
-import asyncio
 import datetime
 import logging
 import threading
@@ -11,7 +10,6 @@ from otto import get_reddit
 from otto import SUBREDDIT_NAME
 from otto.config import Config
 from otto.config import get_config
-from otto.lib.check_posts import check_post
 from otto.lib.nfl_client import NFLClient
 from otto.lib.update_downvote import update_downvote
 from otto.lib.update_sidebar_score import update_sidebar_score
@@ -24,7 +22,6 @@ def main(reddit: Reddit = get_reddit(), sr_name: str = SUBREDDIT_NAME) -> None:
     config = get_config(reddit, sr_name)
 
     run_jobs(config, reddit)
-    check_submissions(config, reddit)
 
 
 def run_jobs(
@@ -53,14 +50,6 @@ def run_jobs(
             update_downvote(config, reddit, sr_name, games)
         except Exception as e:
             logger.exception(e)
-
-
-def check_submissions(config: Config, reddit: Reddit = get_reddit()) -> None:
-    logger.info("Streaming posts: {}".format(datetime.datetime.now()))
-
-    sr = reddit.subreddit(SUBREDDIT_NAME)
-    for post in sr.stream.submissions(skip_existing=True):
-        asyncio.run(check_post(config, post))
 
 
 if __name__ == "__main__":
