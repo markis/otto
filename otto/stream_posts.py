86deba226cfd0925a63f2597df3ff2a053dada66
Markis Taylor <m@rkis.net>
2023-07-06 06:34:22 -0400

    refactor: streamline stream posts

--- otto/stream_posts.py
+++ otto/stream_posts.py
@@ -4,34 +4,28 @@ import logging
 from typing import Final
 
 from asyncpraw.models.reddit.subreddit import Subreddit
-from asyncpraw.reddit import Reddit
 
 from otto import SUBREDDIT_NAME, get_reddit
-from otto.config import Config, get_config
+from otto.config import get_config
 from otto.lib.check_posts import check_post
 
 logger: Final = logging.getLogger(__name__)
 
 
-async def run() -> None:
+def run() -> None:
+    asyncio.run(stream_posts())
+
+
+async def stream_posts() -> None:
     while True:
         async with get_reddit() as reddit:
-            await main(reddit=reddit, sr_name=SUBREDDIT_NAME)
+            config = await get_config(reddit, SUBREDDIT_NAME)
+            logger.info(f"Streaming posts: {datetime.datetime.now()}")
 
-
-async def main(reddit: Reddit, sr_name: str) -> None:
-    config = await get_config(reddit, sr_name)
-
-    await stream_posts(config, reddit)
-
-
-async def stream_posts(config: Config, reddit: Reddit) -> None:
-    logger.info(f"Streaming posts: {datetime.datetime.now()}")
-
-    sr: Subreddit = await reddit.subreddit(SUBREDDIT_NAME)
-    async for post in sr.stream.submissions(skip_existing=True):
-        await check_post(config, post)
+            sr: Subreddit = await reddit.subreddit(SUBREDDIT_NAME)
+            async for post in sr.stream.submissions(skip_existing=True):
+                await check_post(config, post)
 
 
 if __name__ == "__main__":
-    asyncio.run(run())
+    run()
